b6a5109a9abf8dca62fab5ae72070a6f9c22679d
Matias Ramirez <matiaramirez133@gmail.com>
2023-05-14 09:04:07 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://mati-com.vercel.app",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    }),
+    robotsTxt(),
+  ],
+});
---
+++ src/data/info.ts
@@ -0,0 +1,99 @@
+export const info = {
+  baseUrl: "https://github.com/mati-com",
+  name: "Matias Ramirez",
+  jobDescription: "Analista de Sistema | Programador",
+  about: `Técnico superior en análisis de sistemas con una experiencia de 2 años como como soporte técnico. Sin embargo, mi pasión me ha llevado a adentrarme en el mundo de la programación, deseando aprovechar todas las oportunidades para crecer y expandir mis conocimientos en este campo en constante evolución. Estoy entusiasmado por colaborar en proyectos interesantes y aportar mi entusiasmo, dedicación y habilidades al equipo. Mi objetivo es seguir aprendiendo y creciendo profesionalmente. 😀  
+  `,
+
+  experience: [
+
+    {
+      name: "Reparación de Equipos Informáticos (Freelancer).",
+      location: "Formosa, Argentina",
+      startDate: "May 2020",
+      endDate: "Presente",
+      description: [
+        "↬ Habilidades de comunicación.  ",
+        "↬ Gestión eficiente de recursos.",
+        "↬ Conocimientos de electrónica y soldadura.",
+        "↬ Ensamblaje de computadoras personalizadas.",
+      ],
+    },
+
+    {
+      name: "Instituto Argentino de Computación (IAC)",
+      location: "Formosa, Argentina",
+      startDate: "Feb 2018",
+      endDate: "Mzo 2020",
+      description: [
+        "↬ Documentación y seguimiento de incidencias de clientes.",
+        "↬ Virtualización de sistemas operativos y máquinas virtuales.",
+        "↬ Diagnóstico y resolución de problemas técnico de software y hardware.",
+        "↬ Amplio dominio y experiencia en los entornos operativos Windows y Linux.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Instituto Superior de Informática Virasoro (ISIV)",
+      location: "Misiones, Argentina",
+      startDate: "Mar 2020",
+      endDate: "Dic 2022",
+      description: [
+        "Técnico Superior en Análisis de Sistemas",
+        "‎ ",
+        "· Conocimiento en redes.",
+        "· Análisis y diseño de sistemas informáticos.",
+        "· Comprensión de las metodologías de desarrollo de software.",
+      ],
+    },
+
+    {
+      name: "Escuela Provincial Educación Secundaria Nº 3 'Enrique de Ibarreta'",
+      location: "Ibarreta, Formosa, Argentina",
+      startDate: "Mar 2013",
+      endDate: "Dic 2018",
+      description: ["Bachillerato en Ciencias Naturales"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/matys_com",
+    github: "https://github.com/mati-com",
+    email: "mailto:matiaramirez133@gmail.com",
+    linkedin: "https://www.linkedin.com/in/matiram/",
+  },
+
+  projects: [
+    {
+      title: "DecodeThisChallenge",
+      isFeatured: true,
+      thumbnail: "/assets/images/decode.svg",
+      githubUrl: "https://github.com/mati-com/DecodeThisChallenge",
+      liveUrl: "https://mati-com.github.io/DecodeThisChallenge/",
+    },
+
+    {
+      title: "GoLang - CLI",
+      isFeatured: true,
+      thumbnail: "/assets/images/golang.svg",
+      githubUrl: "https://github.com/mati-com/go-cli",
+    },
+    
+    {
+      title: "Caline",
+      isFeatured: true,
+      thumbnail: "/assets/images/caline.svg",
+      githubUrl: "https://github.com/mati-com/caline",
+    },
+
+    {
+      title: "Si, este portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/cv-resume.svg",
+      githubUrl: "https://github.com/mati-com",
+    },
+  ],
+};
